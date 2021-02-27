b0d09b7d18803e01049c065f55381da8799c2032
Florian Grimmer <grimmer.florian@outlook.fr>
2021-02-27 23:01:44 +0100

    ajout des pages de détails des véhicules du catalogue

--- js/car.js
+++ js/car.js
@@ -31,7 +31,7 @@ export let search = function(keywords) {
     return cars.filter(car => car.mod.toLowerCase().includes(keywords.toLowerCase()))
 }
 
-let car1 = new Car(1, 'C63s aMG', "LE PLUS PUISSANT DE SA CATÉGORIE. Un V8 biturbo assemblé main de 503 ch libère un torrent de couple de 516 lb-pi. Un nouveau style agressif est associé à de nouvelles technologies de pointe.", [400, 600, 1200], 'c63s.png', 510, 5, 2018, 'Auto')
+let car1 = new Car(1, 'C63s AMG', "LE PLUS PUISSANT DE SA CATÉGORIE. Un V8 biturbo assemblé main de 503 ch libère un torrent de couple de 516 lb-pi. Un nouveau style agressif est associé à de nouvelles technologies de pointe.", [400, 600, 1200], 'c63s.png', 510, 5, 2018, 'Auto')
 let car2 = new Car(2, 'Lamborghini Aventador', "L’Aventador a été conçue pour aller au-delà du concept de performance, faisant d’emblée figure de référence dans le domaine des supersportives et devançant les voitures du futur. Une famille de supercars déjà inscrite dans la légende.", [600, 900, 1800], 'aventador.png', 700, 2, 2018, 'Auto')
 let car3 = new Car(3, 'Audi R8 v10 plus', "V10 atmosphérique, 620 ch, 580 Nm de couple... Découvrez la plus puissante des Audi de série. Une supercar, à l’aise sur piste comme sur route.", [550, 700, 1600], 'r8.png', 620, 2, 2018, 'Auto')
 let car4 = new Car(4, 'Audi RS6', "Avec son moteur V8 TFSI bi-turbo et ses technologies dernier cri, le break de chasse familial gagne en efficacité. Découvrez le plus intense des breaks Audi jamais produits.", [450, 650, 1300], 'rs6.png', 600, 5, 2018, 'Auto')
